c96d1add70e2a5cbc13d5c107d32eb1d7e4879ff
QA36 <QA36@DESKTOP-AESHFBQ>
2022-12-01 19:44:37 +0100

    NewProductTest

--- SwagProject/SwagProject/Pages/ProductPage.cs
+++ SwagProject/SwagProject/Pages/ProductPage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SwagProject.Driver;
 
 namespace SwagProject.Pages
@@ -15,5 +16,19 @@ namespace SwagProject.Pages
         public IWebElement AddBackPack => driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack"));
         public IWebElement AddT_Shirt => driver.FindElement(By.Id("add-to-cart-sauce-labs-bolt-t-shirt"));
         public IWebElement Cart => driver.FindElement(By.CssSelector("#shopping_cart_container .shopping_cart_badge"));
+
+        public IWebElement SortByByPrice => driver.FindElement(By.ClassName("product_sort_container"));
+
+        public IWebElement MenuClick => driver.FindElement(By.Id("react-burger-menu-btn"));
+
+        public IWebElement AboutClick => driver.FindElement(By.Id("about_sidebar_link"));
+
+        public IWebElement ShoppingCartClick => driver.FindElement(By.Id("shopping_cart_container"));
+
+        public void SelectOptions(string text)
+        {
+            SelectElement element = new SelectElement(SortByByPrice);
+            element.SelectByText(text);
+        }
     }
 }
--- SwagProject/SwagProject/Tests/BuyProductTest.cs
+++ SwagProject/SwagProject/Tests/BuyProductTest.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography.X509Certificates;
+using OpenQA.Selenium;
 using SwagProject.Driver;
 using SwagProject.Pages;
 
@@ -8,6 +9,7 @@ namespace SwagProject.Tests
     {
         LoginPage loginPage;
         ProductPage productPage;
+        CartPage cartPage;
 
         [SetUp]
         public void Setup()
@@ -16,6 +18,7 @@ namespace SwagProject.Tests
             WebDrivers.Initialize();
             loginPage = new LoginPage();
             productPage = new ProductPage();
+            cartPage = new CartPage();  
         }
 
         [TearDown]
@@ -32,8 +35,51 @@ namespace SwagProject.Tests
             productPage.AddT_Shirt.Click();
 
             Assert.That("2",Is.EqualTo(productPage.Cart.Text));
+
+
+        }
+        [Test]
+
+        public void TC02_SortProductByPrice_ShouldSortByHighPrice()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.SelectOptions("Price (high to low)");
+
+            Assert.That(productPage.SortByByPrice.Displayed);
+
         }
 
+        [Test]
+        public void TC03_GoToAboutPage_ShouldRedirectToNewPage()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.MenuClick.Click();
+            productPage.AboutClick.Click();
+
+            Assert.That("https://saucelabs.com/", Is.EqualTo(WebDrivers.Instance.Url));
+        }
+
+        [Test]
+
+       public void TC04_BuyProducts_ShouldBeFinishedShopping()
+        {
+            loginPage.Login("standard_user", "secret_sauce");
+            productPage.AddBackPack.Click();
+            productPage.AddT_Shirt.Click();
+            productPage.ShoppingCartClick.Click();
+
+
+            cartPage.Checkout.Click();
+            cartPage.FirstName.SendKeys("Petar");
+            cartPage.LastName.SendKeys("Matic");
+            cartPage.ZipCode.SendKeys("11000");
+            cartPage.ButonContinue.Submit();
+
+            cartPage.Finish.Click();
+
+            Assert.That("THANK YOU FOR YOUR ORDER", Is.EqualTo(cartPage.OrderFinished.Text));
+        }
+            
     }
 
    
